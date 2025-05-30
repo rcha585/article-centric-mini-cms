0ed547e773bcf0aa4f8396197d7e853c128d86ea
Daoli <dli245@aucklanduni.ac.nz>
2025-05-30 20:31:52 +1200

    login logout

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,5 +1,27 @@
 import express from "express";
+import { getDatabase } from "../data/database";
+import { checkPassword, getTokenFromUsername } from "../data/util";
 
 const router = express.Router();
 
 export default router;
+
+router.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const db = await getDatabase();
+  const user = await db.get("SELECT * FROM users WHERE username = ?", username);
+  if (!user) {
+    return res.sendStatus(400);
+  }
+  const isMatch = await checkPassword(password, user.password);
+  if (!isMatch) {
+    return res.sendStatus(400);
+  }
+  let authToken = getTokenFromUsername(username);
+  return res.cookie("authToken", authToken, {expires: new Date(Date.now() + 24 * 60 * 60 * 1000), path: "/", httpOnly:true}).sendStatus(200);
+});
+
+router.post("/logout", (req, res) => {
+  return res.cookie("authToken", "", {expires: new Date(0), httpOnly: true}).sendStatus(204);
+});
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,21 +2,7 @@ import bcrypt from "bcrypt";
 import express from "express";
 import { getDatabase } from "../data/database.js";
 import yup from "yup";
-
-async function encryptPassword(password) {
-    const encryptedPassword = await bcrypt.hash(password, 10);
-    return encryptedPassword;
-}
-
-const createUserSchema = yup.object({
-  username: yup.string().max(100).required(),
-  password: yup.string().max(100).required(),
-  first_name: yup.string().max(100).required(),
-  last_name: yup.string().max(100).required(),
-  date_of_birth: yup.string().matches(/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, "Invalid date format (YYYY-MM-DD HH:mm:ss)"),
-  description: yup.string().max(255).required(),
-  avatar_path: yup.string().max(255).required()
-}).required();
+import { createUserSchema, encryptPassword } from "../data/util.js"
 
 const router = express.Router();
 
@@ -32,7 +18,7 @@ router.post("/", async (req, res) => {
     return res.sendStatus(201);
   } catch (error) {
     if (error instanceof yup.ValidationError) {
-      return res.send(400).json({errors: error.errors});
+      return res.status(400).json({errors: error.errors});
     } else {
       return res.sendStatus(500);
     }
