1cf67f9e11e921b370103eb6ee62c7d33c9fd0dc
Daoli <dli245@aucklanduni.ac.nz>
2025-05-30 22:07:16 +1200

    login logout me

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getDatabase } from "../data/database";
-import { checkPassword, getTokenFromUsername } from "../data/util";
+import { requiresAuthentication } from "../middleware/authentication.js"
+import { getDatabase } from "../data/database.js";
+import { checkPassword, getTokenFromUsername } from "../data/util.js";
 
 const router = express.Router();
 
@@ -12,11 +13,11 @@ router.post("/login", async (req, res) => {
   const db = await getDatabase();
   const user = await db.get("SELECT * FROM users WHERE username = ?", username);
   if (!user) {
-    return res.sendStatus(400);
+    return res.status(400).send("no user found");
   }
   const isMatch = await checkPassword(password, user.password);
   if (!isMatch) {
-    return res.sendStatus(400);
+    return res.status(400).send("wrong password");
   }
   let authToken = getTokenFromUsername(username);
   return res.cookie("authToken", authToken, {expires: new Date(Date.now() + 24 * 60 * 60 * 1000), path: "/", httpOnly:true}).sendStatus(200);
@@ -24,4 +25,9 @@ router.post("/login", async (req, res) => {
 
 router.post("/logout", (req, res) => {
   return res.cookie("authToken", "", {expires: new Date(0), httpOnly: true}).sendStatus(204);
+});
+
+router.get("/me", requiresAuthentication, async (req, res) => {
+  const {password, ...user} = req.user;
+  return res.status(200).json(user);
 });
