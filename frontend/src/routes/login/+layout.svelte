816dc4ed395a99e7ac33b1bfd04b4785e4a9d559
lshi110 <lshi110@aucklanduni.ac.nz>
2025-06-07 11:00:45 +1200

    I have edit the log-out feature.

--- frontend/src/routes/login/+layout.svelte
+++ frontend/src/routes/login/+layout.svelte
@@ -2,13 +2,3 @@
 <!-- Note: we do NOT import Navbar here. This is a “blank” layout. -->
 <slot />
 
-<style>
-  /* Center the login form, for example */
-  body {
-    background: #ecf0f1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-  }
-</style>
