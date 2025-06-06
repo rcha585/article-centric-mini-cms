0a16245cacc9cc2291a1742b3ad7871308c18af4
Daoli <dli245@aucklanduni.ac.nz>
2025-06-07 00:36:40 +1200

    api added to search comment and corresponding article

--- backend/src/routes/comments.js
+++ backend/src/routes/comments.js
@@ -47,3 +47,34 @@ router.delete("/:cid", requiresAuthentication, async (req, res) => {
   await db.run("DELETE FROM notifications WHERE comment_id = ?", req.params.cid);
   return res.sendStatus(204);
 });
+
+router.get("/:cid", async (req, res) => {
+  const db = await getDatabase();
+  const response = await db.get(`
+    SELECT
+      a.id AS article_id,
+      a.title AS article_title,
+      a.content AS article_content,
+      a.created_at AS article_created_at,
+      a.image_path AS article_image_path,
+      a.author_id AS author_id,
+      au.username AS author_username,
+      (SELECT COUNT(*) FROM subscriptions WHERE subscribed_user_id = a.author_id) AS author_subscriber_count,
+      c.id AS comment_id,
+      c.content AS comment_content,
+      c.created_at AS comment_created_at,
+      c.user_id AS commenter_id,
+      cu.username AS commenter_username,
+      (SELECT COUNT(*) FROM subscriptions WHERE subscribed_user_id = c.user_id) AS commenter_subscriber_count
+    FROM
+      comments AS c
+    INNER JOIN
+      articles AS a ON c.article_id = a.id
+    INNER JOIN
+      users AS au ON a.author_id = au.id
+    INNER JOIN
+      users AS cu ON c.user_id = cu.id
+    WHERE
+      c.id = ?`, req.params.cid);
+  return res.status(200).json(response);
+});
